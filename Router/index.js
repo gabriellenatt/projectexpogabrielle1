b2841358b3ada23dbdf4287fbcdc964394a74626
gabriellenatt <gabriellenatasha525@gmail.com>
2022-05-30 14:35:58 +0700

    perubahan 1

---
+++ Router/index.js
@@ -0,0 +1,13 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Gallery" component={Gallery} />
+    </Stack.Navigator>
+  );
+}
